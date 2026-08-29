f92bc48f8edc3fa82a302540ddf87d2630ca4c8f
JimWei <vvlovqq@gmail.com>
2026-08-30 00:15:53 +0800

    Initial project setup

---
+++ src/main/java/com/agentsaul/AgentSaulApplication.java
@@ -0,0 +1,12 @@
+package com.agentsaul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentSaulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentSaulApplication.class, args);
+    }
+}
